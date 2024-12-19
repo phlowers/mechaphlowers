bdd90838525bd38720d1e314aca533d0d010c975
QUINAOU Aimee <aimee.quinaou@rte-france.com>
2024-12-19 11:15:26 +0100

    feat: Compute x_n and x_m according to the catenary model
    Signed-off-by: QUINAOU Aimee <aimee.quinaou@rte-france.com>

--- src/mechaphlowers/core/models/cable_models.py
+++ src/mechaphlowers/core/models/cable_models.py
@@ -1,10 +1,22 @@
 from abc import ABC, abstractmethod
+from math import asinh, sinh
 
 import numpy as np
 
 
 class GeometricCableModel(ABC):
-    """This abstract class is a base class for various models describing the cable in its own frame."""
+    """This abstract class is a base class for various models describing the cable in its own frame.
+
+    The coordinates are expressed in the cable frame.
+
+    Notes: For now we assume in these geometric models that there's
+    no line angle or wind (or other load on the cable), so we work under the following simplifying assumptions:
+
+    - a = a' = span_length
+    - b = b' = elevation_difference
+
+    Support for line angle and wind will be added later.
+    """
 
     def __init__(
         self, span_length: float, elevation_difference: float, p: float
@@ -15,7 +27,7 @@ class GeometricCableModel(ABC):
 
     @abstractmethod
     def z(self, x: np.ndarray) -> np.ndarray:
-        """Altitude of cable points depending on the abscissa, in the cable frame.
+        """Altitude of cable points depending on the abscissa.
 
         Args:
             x: abscissa
@@ -24,8 +36,30 @@ class GeometricCableModel(ABC):
             altitudes based on the sag tension parameter "p" stored in the model.
         """
 
+    @abstractmethod
+    def x_m(self) -> float: ...
+
+    def x_n(self) -> float:
+        # See above for explanations about following simplifying assumption
+        a_prime = self.elevation_difference
+
+        return a_prime + self.x_m()
+
 
 class CatenaryCableModel(GeometricCableModel):
+    """Implementation of a geometric cable model according to the catenary equation.
+
+    The coordinates are expressed in the cable frame.
+    """
+
     def z(self, x: np.ndarray) -> np.ndarray:
-        """Altitude of cable points in the cable frame, according to the catenary model."""
+        """Altitude of cable points depending on the abscissa."""
         return self.p * (np.cosh(x / self.p) - 1)
+
+    def x_m(self) -> float:
+        p = self.p
+        # See above for explanations about following simplifying assumptions
+        a = self.span_length
+        b = self.elevation_difference
+
+        return -a / 2 + p * asinh(b / (2 * p * sinh(a / (2 * p))))
