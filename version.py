7cf66919e4f0dabd5f4eb26d0263cd4bbdb38471
GOELLER Adrien <adrien.goeller@rte-france.com>
2025-01-21 16:19:08 +0100

    version is working - wainting for ci
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>

--- version.py
+++ version.py
@@ -1,10 +1,20 @@
 from pdm.backend.hooks.version import SCMVersion
+import os
 
 def format_version(version: SCMVersion) -> str:
-    print("welcome in my formatter")
-    print({str(version.version)})
-    print({str(version.distance)})
+    """format_version : function provided for pdm backend to customize the version format
+    In practice, this function is called by the pdm backend to format the version string
+    When called with the PACKAGE_BUILD_TEST environment variable set, it will append a 
+    .dev{PACKAGE_BUILD_TEST} to the version string. This is useful for testing deployment ci.
+    """
+    
+    test_build = os.getenv("PACKAGE_BUILD_TEST","")
+    
+    dev_str = ""
+    if test_build != "":
+        dev_str = f".dev{test_build}"
+    
     if version.distance is None:
-        return str(version.version)
+        return str(version.version)+dev_str
     else:
-        return f"{version.version}.post{version.distance}"
+        return f"{version.version}.post{version.distance}"+dev_str
