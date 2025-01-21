9cb7532335768c56ae92bf87d70bee3678ffd3fd
GOELLER Adrien <adrien.goeller@rte-france.com>
2025-01-21 16:19:46 +0100

    doc addition
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>

--- version.py
+++ version.py
@@ -2,10 +2,13 @@ from pdm.backend.hooks.version import SCMVersion
 import os
 
 def format_version(version: SCMVersion) -> str:
-    """format_version : function provided for pdm backend to customize the version format
+    """format_version : function provided for pdm backend to customize the version format when extract from scm tag.
     In practice, this function is called by the pdm backend to format the version string
     When called with the PACKAGE_BUILD_TEST environment variable set, it will append a 
     .dev{PACKAGE_BUILD_TEST} to the version string. This is useful for testing deployment ci.
+    
+    Note that this function is not called if the PDM_BUILD_SCM_VERSION environment variable is set.
+    In this case there is no control from this function but only from pdm backend.
     """
     
     test_build = os.getenv("PACKAGE_BUILD_TEST","")
