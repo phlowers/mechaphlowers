f4ee2491fb706b2ffa71bfa83a57b8d808b32ae9
GOELLER Adrien <adrien.goeller@rte-france.com>
2025-01-27 12:09:01 +0100

    fix
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 import os
 
-from pdm.backend.hooks.version import SCMVersion # type: ignore
+from pdm.backend.hooks.version import SCMVersion  # type: ignore
 
 
 def format_version(version: SCMVersion) -> str:
