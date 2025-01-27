71a0332c0680e8d30f6a3581ddd2e9e65253541a
GOELLER Adrien <adrien.goeller@rte-france.com>
2025-01-27 11:59:57 +0100

    fix
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>

--- version.py
+++ version.py
@@ -12,14 +12,19 @@ def format_version(version: SCMVersion) -> str:
 	Note that this function is not called if the PDM_BUILD_SCM_VERSION environment variable is set.
 	In this case there is no control from this function but only from pdm backend.
 	"""
-
+	print("---- pdm custom build version ----")
+	print(f"INFO: Version tag: {version.version}",)
 	test_build = os.getenv("PACKAGE_BUILD_TEST", "")
 
 	dev_str = ""
 	if test_build != "":
 		dev_str = f".dev{test_build}"
-
-	if version.distance is None:
+		print(f"INFO Test build is activated, append {dev_str} to version")
 		return str(version.version) + dev_str
+
+	print("INFO Final version build")
+	if version.distance is None:
+		return str(version.version)
 	else:
-		return f"{version.version}.post{version.distance}" + dev_str
+		print(f"INFO Version is a post release, append .post{version.distance} to version")
+		return f"{version.version}.post{version.distance}"
