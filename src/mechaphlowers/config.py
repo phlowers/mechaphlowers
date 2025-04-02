831fe9dbfc82dfd4a574219fa6613b11ee003b1d
GOELLER Adrien <adrien.goeller@rte-france.com>
2025-04-02 16:11:12 +0200

    fixes
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>

--- src/mechaphlowers/config.py
+++ src/mechaphlowers/config.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2025, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+
 """Module for mechaphlowers configuration settings"""
 
 from dataclasses import dataclass
@@ -13,7 +19,7 @@ class GraphicsConfig:
 
 @dataclass
 class SolverConfig:
-	"""Solver configuration class."""
+	"""Solvers configuration class."""
 
 	sagtension_zeta: int = 10
 	deformation_imag_thresh: float = 1e-5
