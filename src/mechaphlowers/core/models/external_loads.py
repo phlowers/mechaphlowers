0f03ed191890c5cb807b92e494f236186cd0fccc
WANG Jérémy <jeremy.wang@rte-france.com>
2025-05-05 16:02:06 +0200

    create converter
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- src/mechaphlowers/core/models/external_loads.py
+++ src/mechaphlowers/core/models/external_loads.py
@@ -4,8 +4,9 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # SPDX-License-Identifier: MPL-2.0
 
+import math
 from math import pi
-from typing import Any
+from typing import Any, Literal
 
 import numpy as np
 
@@ -103,3 +104,66 @@ class CableLoads:
         for key, value in data.items():
             if hasattr(self, key):
                 setattr(self, key, value)
+
+
+class WindSpeedPressureConverter:
+    def __init__(
+        # allow floats?
+        self,
+        wind_angle_cable_degrees: np.ndarray,
+        tower_height: np.float64,  # or np.ndarray?
+        voltage: np.float64,  # np.ndarray?
+        category_surface_roughness: Literal["0", "II", "III"] = "II",  # IIIa?
+        gust_wind: np.ndarray | None = None,
+        speed_average_wind_open_country: np.ndarray | None = None,
+        work: bool = False,
+    ):
+        self.wind_angle_cable_degrees = wind_angle_cable_degrees
+        self.tower_height = tower_height
+        self.voltage = voltage
+        self.category_surface_roughness = category_surface_roughness
+        if speed_average_wind_open_country is None:
+            if gust_wind is None:
+                raise TypeError(
+                    "gust_wind or speed_average_wind_open_country need to be not"
+                )
+            else:
+                speed_average_wind_open_country = gust_wind / 1.54 / 3.6
+        # if gust_wind and speed_average_wind_open_country are both given, speed_average_wind_open_country is used
+        self.gust_wind = gust_wind
+        self._speed_average_wind_open_country = speed_average_wind_open_country
+        self.work = work
+
+    @property
+    def speed_average_wind_open_country(self):
+        return np.round(self._speed_average_wind_open_country, 1)
+
+    def get_pressure(self) -> np.ndarray:
+        if self.voltage <= 90:
+            h = self.tower_height * 3 / 4
+        else:
+            h = self.tower_height * 2 / 3
+
+        roughness = {"0": 0.005, "II": 0.05, "III": 0.2}
+        length = np.float64(roughness[self.category_surface_roughness])
+
+        k: np.float64 = 0.19 * (length / 0.05) ** 0.07
+        V = (
+            self._speed_average_wind_open_country
+            * k
+            * np.log(h / length)
+            * np.sin(self.wind_angle_cable_degrees / 180 * math.pi)
+        )
+        Iv: np.float64 = 1 / np.log(h / length)
+
+        work_coefficient = 1.0
+        if self.work is True:
+            work_coefficient = 1.2
+
+        wind_load_pa = (
+            0.5 * 1.25 * V**2 * (1 + 7 * Iv) * 2 / 3 * work_coefficient
+        )
+        return wind_load_pa
+
+    def get_pressure_rounded(self) -> np.ndarray:
+        return np.round(self.get_pressure() / 10) * 10
