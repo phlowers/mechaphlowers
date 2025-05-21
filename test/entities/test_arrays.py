9b21d0b0267359bd14535b2a18417aca1e4ff59d
WANG Jérémy <jeremy.wang@rte-france.com>
2025-05-21 12:07:15 +0200

    Replace pdt with pd DataFrame
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- test/entities/test_arrays.py
+++ test/entities/test_arrays.py
@@ -10,13 +10,10 @@ import pandera as pa
 import pytest
 from numpy.testing import assert_allclose
 from pandas.testing import assert_frame_equal
-from pandera.typing import pandas as pdt
 
 from mechaphlowers.entities.arrays import (
     CableArray,
-    CableArrayInput,
     SectionArray,
-    SectionArrayInput,
     WeatherArray,
 )
 
@@ -36,7 +33,7 @@ def section_array_input_data() -> dict[str, list]:
 
 @pytest.fixture
 def section_array(section_array_input_data: dict[str, list]) -> SectionArray:
-    df = pdt.DataFrame[SectionArrayInput](section_array_input_data)
+    df = pd.DataFrame(section_array_input_data)
     return SectionArray(
         data=df, sagging_parameter=2_000, sagging_temperature=15
     )
@@ -67,9 +64,7 @@ def cable_array_input_data() -> dict[str, list]:
 def test_create_section_array__with_floats(
     section_array_input_data: dict,
 ) -> None:
-    input_df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    input_df = pd.DataFrame(section_array_input_data)
     section = SectionArray(
         input_df, sagging_parameter=2_000, sagging_temperature=15
     )
@@ -78,7 +73,7 @@ def test_create_section_array__with_floats(
 
 
 def test_create_section_array__only_ints() -> None:
-    input_df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
+    input_df = pd.DataFrame(
         {
             "name": ["support 1", "2", "three", "support 4"],
             "suspension": [False, True, True, False],
@@ -100,9 +95,7 @@ def test_create_section_array__span_length_for_last_support(
     section_array_input_data: dict,
 ) -> None:
     section_array_input_data["span_length"][-1] = 300
-    input_df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    input_df = pd.DataFrame(section_array_input_data)
 
     with pytest.raises(pa.errors.SchemaErrors):
         SectionArray(input_df, sagging_parameter=2_000, sagging_temperature=15)
@@ -124,9 +117,7 @@ def test_create_section_array__missing_column(
     section_array_input_data: dict, column: str
 ) -> None:
     del section_array_input_data[column]
-    input_df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    input_df = pd.DataFrame(section_array_input_data)
 
     with pytest.raises(pa.errors.SchemaErrors):
         SectionArray(input_df, sagging_parameter=2_000, sagging_temperature=15)
@@ -137,9 +128,7 @@ def test_create_section_array__extra_column(
 ) -> None:
     section_array_input_data["extra column"] = [0] * 4
 
-    input_df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    input_df = pd.DataFrame(section_array_input_data)
 
     section = SectionArray(
         input_df, sagging_parameter=2_000, sagging_temperature=15
@@ -164,9 +153,7 @@ def test_create_section_array__wrong_type(
     section_array_input_data: dict, column: str, value
 ) -> None:
     section_array_input_data[column] = value
-    input_df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    input_df = pd.DataFrame(section_array_input_data)
 
     with pytest.raises(pa.errors.SchemaErrors):
         SectionArray(input_df, sagging_parameter=2_000, sagging_temperature=15)
@@ -177,9 +164,7 @@ def test_create_section_array__insulator_length_for_tension_support(
 ) -> None:
     section_array_input_data["suspension"] = [False, False, True, False]
     section_array_input_data["insulator_length"] = [0.5, 0.5, 0.5, 0.5]
-    input_df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    input_df = pd.DataFrame(section_array_input_data)
 
     with pytest.raises(pa.errors.SchemaErrors):
         SectionArray(input_df, sagging_parameter=2_000, sagging_temperature=15)
@@ -203,7 +188,7 @@ def test_compute_elevation_difference() -> None:
         "span_length": [50, 100, 500, np.nan],
     }
 
-    df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(data)
+    df = pd.DataFrame(data)
 
     section_array = SectionArray(
         df, sagging_parameter=2_000, sagging_temperature=15
@@ -217,9 +202,7 @@ def test_compute_elevation_difference() -> None:
 
 
 def test_section_array__data(section_array_input_data: dict) -> None:
-    df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    df = pd.DataFrame(section_array_input_data)
     section_array = SectionArray(
         data=df, sagging_parameter=2_000, sagging_temperature=15
     )
@@ -250,9 +233,7 @@ def test_section_array__data(section_array_input_data: dict) -> None:
 def test_section_array__data_without_sagging_properties(
     section_array_input_data: dict,
 ) -> None:
-    df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    df = pd.DataFrame(section_array_input_data)
 
     section_array_without_temperature = SectionArray(
         data=df, sagging_parameter=2_000
@@ -268,9 +249,7 @@ def test_section_array__data_without_sagging_properties(
 
 
 def test_section_array__data_original(section_array_input_data: dict) -> None:
-    df: pdt.DataFrame[SectionArrayInput] = pdt.DataFrame(
-        section_array_input_data
-    )
+    df = pd.DataFrame(section_array_input_data)
     section_array = SectionArray(data=df)
 
     exported_data = section_array.data_original
@@ -337,9 +316,7 @@ def test_create_cable_array__missing_column(
     cable_array_input_data: dict, column: str
 ) -> None:
     del cable_array_input_data[column]
-    input_df: pdt.DataFrame[CableArrayInput] = pdt.DataFrame(
-        cable_array_input_data
-    )
+    input_df = pd.DataFrame(cable_array_input_data)
 
     with pytest.raises(pa.errors.SchemaErrors):
         CableArray(input_df)
@@ -360,9 +337,7 @@ def test_create_cable_array__wrong_type(
     cable_array_input_data: dict, column: str, value
 ) -> None:
     cable_array_input_data[column] = value
-    input_df: pdt.DataFrame[CableArrayInput] = pdt.DataFrame(
-        cable_array_input_data
-    )
+    input_df = pd.DataFrame(cable_array_input_data)
 
     with pytest.raises(pa.errors.SchemaErrors):
         CableArray(input_df)
@@ -373,9 +348,7 @@ def test_create_cable_array__extra_column(
 ) -> None:
     cable_array_input_data["extra column"] = [0]
 
-    input_df: pdt.DataFrame[CableArrayInput] = pdt.DataFrame(
-        cable_array_input_data
-    )
+    input_df = pd.DataFrame(cable_array_input_data)
 
     section = CableArray(input_df)
 
