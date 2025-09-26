526c639824de37b75b53fcbe39add74bcc322643
WANG Jérémy <jeremy.wang@rte-france.com>
2025-09-26 17:27:51 +0200

    after review
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- test/balance/test_balance_numeric.py
+++ test/balance/test_balance_numeric.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# from https://github.com/NKrvavica/fqs/tree/master MIT Licence
 """
 Created on Thu Jan  3 11:14:52 2019
 
@@ -29,13 +29,6 @@ def eig_cubic_roots(p):
 
 
 def test_cubic_roots():
-    # import plotly.graph_objects as go
-
-    # fig = go.Figure()
-    # x = np.arange(-5, 5, 0.01, dtype=float)
-    # y = -1*x**3 -3*x**2 +2*x -0
-    # fig.add_trace(go.Scatter(x=x,y=y))
-
     p0 = np.array([5, 0.1, 0.01, 0.00001, 1e-10])
     p1 = np.array((2, 2, 0, 0, 0))
     p2 = np.array([-3, -3, -3, 0, 0])
@@ -66,81 +59,3 @@ def test_cubic_roots():
     np.testing.assert_array_almost_equal(
         r, np.array([1.0, 2.17988479, 17.95219749, 73.68062997, 0.0])
     )
-
-
-# fig.show()
-
-
-# # --------------------------------------------------------------------------- #
-# # Test speed of fqs cubic solver compared to np.roots and np.linalg.eigvals
-# # --------------------------------------------------------------------------- #
-
-# # Number of samples (sets of randomly generated cubic coefficients)
-# N = 100
-
-# # Generate polynomial coefficients
-# range_coeff = 100
-# p = np.random.rand(N, 4)*(range_coeff) - range_coeff/2
-
-# # number of runs
-# runs = 10
-
-# times = []
-# for i in range(runs):
-#     start = timeit.default_timer()
-#     roots1 = [np.roots(pi) for pi in p]
-#     stop = timeit.default_timer()
-#     time = stop - start
-#     times.append(time)
-# print('np.roots: {:.4f} ms (best of {} runs)'
-#       .format(np.array(times).mean()*1_000, runs))
-
-# times = []
-# for i in range(runs):
-#     start = timeit.default_timer()
-#     roots2 = eig_cubic_roots(p)
-#     stop = timeit.default_timer()
-#     time = stop - start
-#     times.append(time)
-# print('np.linalg.eigvals: {:.4f} ms (average of {} runs)'
-#       .format(np.array(times).mean()*1_000, runs))
-# print('max err: {:.2e}'.format(abs(np.sort(roots2, axis=1)
-#                     - (np.sort(roots1, axis=1))).max()))
-
-# times = []
-# for i in range(runs):
-#     start = timeit.default_timer()
-#     roots3 = [fqs.single_cubic(*pi) for pi in p]
-#     stop = timeit.default_timer()
-#     time = stop - start
-#     times.append(time)
-# print('fqs.single_cubic: {:.4f} ms (average of {} runs)'
-#       .format(np.array(times).mean()*1_000, runs))
-# print('max err: {:.2e}'.format(abs(np.sort(roots3, axis=1)
-#                     - (np.sort(roots1, axis=1))).max()))
-
-# times = []
-# for i in range(runs):
-#     start = timeit.default_timer()
-#     roots = fqs.multi_cubic(*p.T)
-#     roots4 = np.array(roots).T
-#     stop = timeit.default_timer()
-#     time = stop - start
-#     times.append(time)
-# print('fqs.multi_cubic: {:.4f} ms (average of {} runs)'
-#       .format(np.array(times).mean()*1_000, runs))
-# print('max err: {:.2e}'.format(abs(np.sort(roots4, axis=1)
-#                     - (np.sort(roots1, axis=1))).max()))
-
-# times = []
-# for i in range(runs):
-#     start = timeit.default_timer()
-#     roots5 = fqs.cubic_roots(p)
-#     stop = timeit.default_timer()
-#     time = stop - start
-#     times.append(time)
-# print('fqs.cubic_roots: {:.4f} ms (average of {} runs)'
-#       .format(np.array(times).mean()*1_000, runs))
-# print('max err: {:.2e}'.format(abs(np.sort(roots5, axis=1)
-#                     - (np.sort(roots1, axis=1))).max()))
-# # --------------------------------------------------------------------------- #
