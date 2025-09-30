f8ab6408821963fed1f3e23f96207246e6b1f6c1
GOELLER Adrien <adrien.goeller@rte-france.com>
2025-09-30 15:58:54 +0200

    wip poc on np cache
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>

--- src/mechaphlowers/utils.py
+++ src/mechaphlowers/utils.py
@@ -4,6 +4,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # SPDX-License-Identifier: MPL-2.0
 
+from functools import lru_cache, wraps
 import logging
 from typing import Callable
 
@@ -149,3 +150,49 @@ def np_cache(*args, **kwargs):
         return wrapper
 
     return decorator
+
+
+import hashlib
+import time
+
+import numpy as np
+
+from functools import lru_cache, wraps
+
+# def expensive_function(x: np.array)-> float:
+#     time.sleep(0.1)
+#     return np.sqrt(x.clip(1).sum())
+
+class YetAnotherWrapper:
+    def __init__(self, x: np.array) -> None:
+        self.values = x
+        # here you can use your own hashing function
+        self.h = hashlib.sha224(x.view()).hexdigest()
+
+    def __hash__(self) -> int:
+        return hash(self.h)
+    
+    def __eq__(self, __value: object) -> bool:
+        return __value.h == self.h
+
+def memoizer(expensive_function):
+    @lru_cache()
+    def cached_wrapper(shell):
+        return expensive_function(shell.values)
+
+    @wraps(expensive_function)
+    def wrapper(*args):
+        shell = YetAnotherWrapper(args)
+        return cached_wrapper(shell)
+
+    return wrapper
+
+
+# a = np.random.random((1, 5))
+# a = np.vstack((a, )* 200)
+
+# memoized_expensive_function = memoizer(expensive_function)
+
+# np.apply_along_axis(expensive_function, 1, a) # takes 20 seconds
+# np.apply_along_axis(memoized_expensive_function, 1, a) # takes 0.2 seconds
+
--- test/test_utils.py
+++ test/test_utils.py
@@ -5,9 +5,13 @@
 # SPDX-License-Identifier: MPL-2.0
 
 
+import hashlib
+from time import sleep
 import numpy as np
 
-from mechaphlowers.utils import CachedAccessor, ppnp
+from mechaphlowers.utils import CachedAccessor, memoizer, ppnp
+from functools import wraps, lru_cache
+from mechaphlowers.utils import np_cache
 
 # FILE: src/mechaphlowers/test_utils.py
 
@@ -58,3 +62,69 @@ def test_ppnp_high_precision(capsys) -> None:
     ppnp(arr, prec=4)
     captured = capsys.readouterr()
     assert captured.out == "[1.1235 2.1235 3.1235]\n"
+    # def test_np_cache():
+
+    @np_cache(maxsize=256)
+    def multiply(array, factor):
+        sleep(5)
+        return factor * array
+
+    array = np.array([1, 2, 3,4, 5, 6])
+    
+    @np_cache(maxsize=256)
+    def cached_x_m(
+        a: np.ndarray,
+        b: np.ndarray,
+        p: np.ndarray,
+    ) -> np.ndarray:
+
+        return -a / 2 + p * np.arcsinh(b / (2 * p * np.sinh(a / (2 * p))))
+    
+    def x_m(
+        a: np.ndarray,
+        b: np.ndarray,
+        p: np.ndarray,
+    ) -> np.ndarray:
+
+        return -a / 2 + p * np.arcsinh(b / (2 * p * np.sinh(a / (2 * p))))
+    
+    a = np.array([501.3, 499.0]*100)  # test here int and float
+    b = np.array([0.0, -5.0]*100)
+    p = np.array([2_112.2, 2_112.0]*100)
+    
+    import timeit
+    
+    
+    timeit.timeit(memoizer(x_m(a, b, p)))
+    timeit.timeit(cached_x_m(a, b, p))
+    timeit.timeit(x_m(a, b, p))
+    
+    
+    hashlib.sha256( a.data )
+        
+    # Test basic functionality
+    result1 = multiply(array, 2)
+    np.testing.assert_array_equal(result1, array * 2)
+    
+    # Test caching - second call should hit cache
+    result2 = multiply(array, 2) 
+    assert multiply.cache_info().hits == 1
+    assert multiply.cache_info().misses == 1
+    
+    # Test cache miss with different args
+    result3 = multiply(array, 3)
+    assert multiply.cache_info().hits == 1 
+    assert multiply.cache_info().misses == 2
+    
+    # Test cache works with same array data but different object
+    array2 = np.array([[1, 2, 3], [4, 5, 6]])
+    result4 = multiply(array2, 2)
+    assert multiply.cache_info().hits == 2
+    assert multiply.cache_info().misses == 2
+    
+    # Test cache clear
+    multiply.cache_clear()
+    result5 = multiply(array, 2)
+    assert multiply.cache_info().hits == 0
+    assert multiply.cache_info().misses == 1
+
