25eec2dc454afe4f33989b521a0d83528971d354
adriengoeller <adrien.goeller@rte-france.com>
2025-10-31 14:01:45 +0100

    Feature/#293/add helpers/from main (#295)
    * change state input logic
    * add sample section factory in catalog
    * add figure factory
    * docstring needed for user
    * fix format lint tests
    
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>
    
    * fix after review
    
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>
    
    ---------
    
    Signed-off-by: GOELLER Adrien <adrien.goeller@rte-france.com>

--- src/mechaphlowers/plotting/__init__.py
+++ src/mechaphlowers/plotting/__init__.py
@@ -1,3 +1,10 @@
-from mechaphlowers.plotting.plot import PlotEngine
+# Copyright (c) 2025, RTE (http://www.rte-france.com)
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
 
-__all__ = ["PlotEngine"]
+
+from mechaphlowers.plotting.plot import PlotEngine, figure_factory
+
+__all__ = ["PlotEngine", "figure_factory"]
