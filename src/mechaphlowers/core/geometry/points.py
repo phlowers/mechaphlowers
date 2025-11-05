remove commentated code + fix sign angle
Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

src/mechaphlowers/core/geometry/points.py
@@ -501,7 +501,7 @@ class SectionPoints:
         frame_index: int,
     ):
         # angle_to_project =  self.plane.azimuth_angle[frame_index]
-        angle_to_project = -np.cumsum(self.line_angle)[frame_index]
+        angle_to_project = np.cumsum(self.line_angle)[frame_index]
         translation_vector = -supports_points.coords[frame_index, 0]
 
         new_span = self.change_frame(

src/mechaphlowers/core/geometry/references.py
@@ -119,8 +119,8 @@ def project_coords(
     x1: np.ndarray, y1: np.ndarray, azimuth_angle: np.float64
 ) -> Tuple[np.ndarray, np.ndarray]:
     # formula specifically if frame 1 is rotated from frame 0 with angle azimuth_angle
-    x0 = np.cos(azimuth_angle) * x1 - np.sin(azimuth_angle) * y1
-    y0 = np.sin(azimuth_angle) * x1 + np.cos(azimuth_angle) * y1
+    x0 = np.cos(azimuth_angle) * x1 + np.sin(azimuth_angle) * y1
+    y0 = -np.sin(azimuth_angle) * x1 + np.cos(azimuth_angle) * y1
     return x0, y0
 
 