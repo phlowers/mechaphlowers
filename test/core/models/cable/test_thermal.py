dfb6b1095d1c1b9154091012b455bce6094ff19e
WANG Jérémy <jeremy.wang@rte-france.com>
2025-12-01 09:51:50 +0100

    allow arrays for input
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- test/core/models/cable/test_thermal.py
+++ test/core/models/cable/test_thermal.py
@@ -5,9 +5,61 @@
 # SPDX-License-Identifier: MPL-2.0
 
 
+import numpy as np
+
 from mechaphlowers.core.models.cable.thermal import get_cable_temperature
 from mechaphlowers.entities.arrays import CableArray
 
 
-def test_thermohl_cable_temp(cable_array_AM600: CableArray):
-    get_cable_temperature(cable_array_AM600)
+def test_thermohl_cable_temp_floats(cable_array_AM600: CableArray):
+    get_cable_temperature(
+        cable_array_AM600,
+        latitude=45.0,
+        longitude=0.0,
+        altitude=0.0,
+        azimuth=0.0,
+        month=3,
+        day=21,
+        hour=12,
+        intensity=100.0,
+        ambient_temp=15.0,
+        wind_speed=0.0,
+        wind_angle=90.0,
+    )
+
+
+def test_thermohl_cable_temp_arrays(cable_array_AM600: CableArray):
+    get_cable_temperature(
+        cable_array_AM600,
+        latitude=np.array([45.0, 45.0]),
+        longitude=np.array([0.0, 0.0]),
+        altitude=np.array([0.0, 0.0]),
+        azimuth=np.array([0.0, 0.0]),
+        month=np.array(
+            [
+                3,
+                3,
+            ]
+        ),
+        day=np.array(
+            [
+                21,
+                21,
+            ]
+        ),
+        hour=np.array(
+            [
+                12,
+                12,
+            ]
+        ),
+        intensity=np.array([100.0, 100.0]),
+        ambient_temp=np.array([15.0, 15.0]),
+        wind_speed=np.array([0.0, 0.0]),
+        wind_angle=np.array(
+            [
+                90.0,
+                90.0,
+            ]
+        ),
+    )
