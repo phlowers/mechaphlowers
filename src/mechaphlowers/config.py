7e6adba5e3431151a83c026de7cf7ff129f844c8
WANG Jérémy <jeremy.wang@rte-france.com>
2026-01-08 15:07:20 +0100

    Fix bug dtype('float64') instead of float64
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- src/mechaphlowers/config.py
+++ src/mechaphlowers/config.py
@@ -27,8 +27,8 @@ class OutputUnitsConfig:
 class PrecisionConfig:
     """Precision configuration class."""
 
-    dtype_float: Type[np.floating] = np.float64
-    dtype_int: Type[np.integer] = np.int64
+    dtype_float: np.typing.DTypeLike = np.dtype('float64')
+    dtype_int: np.typing.DTypeLike = np.dtype('int64')
 
 
 @dataclass
