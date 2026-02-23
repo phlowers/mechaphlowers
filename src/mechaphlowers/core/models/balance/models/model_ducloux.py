8558aeabbf9ca06c8f62fa4934a606be91cc1a4b
WANG Jérémy <jeremy.wang@rte-france.com>
2026-02-23 17:43:34 +0100

    List -> list, same with Dict and Tuple
    Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

--- src/mechaphlowers/core/models/balance/models/model_ducloux.py
+++ src/mechaphlowers/core/models/balance/models/model_ducloux.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 
 import logging
 from copy import copy
-from typing import Tuple, Type
+from typing import Type
 
 import numpy as np
 import pandas as pd
@@ -211,12 +211,12 @@ class BalanceModel(IBalanceModel):
 
     def compute_Th_and_extremum(
         self,
-    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """Run FindParamSolver to compute parameter, and then compute Th, x_m and x_n.
         Used only in change_state case.
 
         Returns:
-            Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: Th, x_m, x_n, parameter
+            tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: Th, x_m, x_n, parameter
         """
         # First run approx_parameter to have a closer first value of the parameter. This uses the parabola model.
         parameter_parabola = approx_parameter(
--- src/mechaphlowers/data/initializer/array_importer.py
+++ src/mechaphlowers/data/initializer/array_importer.py
@@ -8,7 +8,6 @@
 from abc import ABC, abstractmethod
 from os import PathLike
 from pathlib import Path
-from typing import Tuple
 
 import numpy as np
 import pandas as pd
@@ -132,6 +131,6 @@ class ImporterRte(Importer):
 
 def import_data_from_proto(
     filename: str | PathLike,
-) -> Tuple[SectionArray, CableArray, WeatherArray]:
+) -> tuple[SectionArray, CableArray, WeatherArray]:
     importer = ImporterRte(filename)
     return importer.section_array, importer.cable_array, importer.weather_array
