(almost) fix angles units
Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

src/mechaphlowers/data/geography/helpers.py
@@ -206,33 +206,36 @@ def reverse_haversine(
 
 
 def reverse_haversine_float(
-    lat: float,
-    lon: float,
+    lat_rad: float,
+    lon_rad: float,
     bearing: float,
     distance: float,
 ) -> tuple[float, float]:
-    """
-    Reverse of haversine with floats.
-
-    Get starting latitude and longitude in radians
+    """Reverse of haversine with floats.
 
     Returns next coordinates in lat/lon according to bearing and distance.
 
-    Angle values in rad.
+    Args:
+        lat (float): starting latitude in radians
+        lon (float): starting longitude in radians
+        bearing (float): bearing angle: orientation of the line in radians, anticlockwise, towards north = 0
+        distance (float): distance with the next point
+
+    Returns:
+        tuple[float, float]: (lat2, lon2): GPS coordinates of next point
     """
-    # TODO: fix bearing sense
     angdist = distance / RADIUS_EARTH
 
-    lat2 = np.arcsin(
-        np.sin(lat) * np.cos(angdist)
-        + np.cos(lat) * np.sin(angdist) * np.cos(bearing)
+    lat_rad_2 = np.arcsin(
+        np.sin(lat_rad) * np.cos(angdist)
+        + np.cos(lat_rad) * np.sin(angdist) * np.cos(bearing)
     )
 
-    lon2 = lon + np.arctan2(
-        np.sin(bearing) * np.sin(angdist) * np.cos(lat),
-        np.cos(angdist) - np.sin(lat) * np.sin(lat2),
+    lon_rad_2 = lon_rad + np.arctan2(
+        -np.sin(bearing) * np.sin(angdist) * np.cos(lat_rad),
+        np.cos(angdist) - np.sin(lat_rad) * np.sin(lat_rad_2),
     )
-    return (lat2, lon2)
+    return (lat_rad_2, lon_rad_2)
 
 
 def haversine(
@@ -287,9 +290,8 @@ def gps_to_bearing(
         unit (Literal["rad", "deg"]): Select the unit to use for both inputs and output. Defaults to "rad"
 
     Returns:
-        np.ndarray: Bearing angle in degrees from north (in radians by default)
+        np.ndarray: Bearing angle in degrees from north, anti clockwise (in radians by default)
     """
-    # TODO: fix sense of bearing output (maybe ok?)
     if unit == "deg":
         lat1, lon1, lat2, lon2 = np.radians([lat1, lon1, lat2, lon2])
     dlon = lon2 - lon1
@@ -297,8 +299,9 @@ def gps_to_bearing(
     x = np.cos(lat1) * np.sin(lat2) - np.sin(lat1) * np.cos(lat2) * np.cos(
         dlon
     )
-    bearing = np.arctan2(y, x)
-    bearing = bearing % (2 * np.pi)
+    # This formula needs to be reversed to get a anti clockwise angle
+    bearing = -np.arctan2(y, x)
+
     if unit == "deg":
         bearing = np.degrees(bearing)
     return bearing

src/mechaphlowers/entities/arrays.py
@@ -234,9 +234,9 @@ class SectionArray(ElementArray):
         start_longitude: float,
         start_azimuth: float,
     ) -> tuple[np.ndarray, np.ndarray]:
-        # TODO: degrees here
+        # TODO: docstring
         line_angle_geo_degrees = (
-            -Q_(self.data["line_angle"].to_numpy(), "rad").to("deg").m
+            Q_(self.data["line_angle"].to_numpy(), "rad").to("deg").m
         )
         return get_gps_from_arrays(
             start_latitude,

src/mechaphlowers/entities/geography.py
@@ -57,7 +57,8 @@ def geo_info_from_gps(lats: np.ndarray, lons: np.ndarray) -> SupportGeoInfo:
 
 
 def get_dist_and_angles_from_gps(
-    latitudes_deg: np.ndarray, longitudes_deg: np.ndarray
+    latitudes_deg: np.ndarray,
+    longitudes_deg: np.ndarray,
 ) -> tuple[np.ndarray, np.ndarray]:
     """Compute distances and angles between supports using latitudes and longitudes.
 
@@ -66,8 +67,9 @@ def get_dist_and_angles_from_gps(
         longitudes (np.ndarray): array of longitudes in decimal degrees
 
     Returns:
-        tuple[np.ndarray, np.ndarray]: tuple (distance, angles) in meters and decimal degrees
+        tuple[np.ndarray, np.ndarray]: tuple (distance, angles) distance is in meters and angles is anti-clockwise
     """
+    # TODO: unit as argument
     lats_rolled_rad = np.radians(latitudes_deg[1:])
     lons_rolled_rad = np.radians(longitudes_deg[1:])
 
@@ -84,7 +86,8 @@ def get_dist_and_angles_from_gps(
     angles_rad = np.diff(bearings_rad)
     angles_rad = np.concatenate(([0], angles_rad, [0]))
 
-    return distances, np.degrees(angles_rad)
+    geo_angles_deg = np.degrees(angles_rad)
+    return distances, geo_angles_deg
 
 
 def get_gps_from_arrays(
@@ -108,14 +111,13 @@ def get_gps_from_arrays(
     Args:
         start_lat_deg (float): latitude of the first point
         start_lon_deg (float): longitude of the first point
-        azimuth_deg (float): azimuth of the first span in geometrical degrees
-        line_angles_degrees (np.ndarray): line angle array (data from SectionArray), in geometrical degrees
+        azimuth_deg (float): azimuth of the first span in degrees, anti-clockwise. 0 means North, 90 means West.
+        line_angles_degrees (np.ndarray): line angle array (data from SectionArray), in degrees, anti-clockwise
         span_length (np.ndarray): span length array (data from SectionArray)
 
     Returns:
-        tuple[np.ndarray, np.ndarray]: (lat, lon) two arrays of angles in degrees
+        tuple[np.ndarray, np.ndarray]: (lat, lon) two arrays of GPS coordinates. Angles in degrees
     """
-    # TODO: fix docstring -> geometric degrees?
     current_lat_rad = np.radians(start_lat_deg)
     current_lon_rad = np.radians(start_lon_deg)
     lat_array_rad = [current_lat_rad]

test/entities/test_geography.py
@@ -37,7 +37,7 @@ def test_section_array_to_gps_0():
         start_lat_deg=48.8566,
         start_lon_deg=2.3522,
         azimuth_deg=0,
-        line_angles_degrees=-np.degrees(
+        line_angles_degrees=np.degrees(
             section_array.data.line_angle.to_numpy()
         ),
         span_length=section_array.data.span_length.to_numpy(),
@@ -72,13 +72,13 @@ def test_section_array_to_gps_1():
         start_lat_deg=48.8566,
         start_lon_deg=2.3522,
         azimuth_deg=0,
-        line_angles_degrees=-np.degrees(
+        line_angles_degrees=np.degrees(
             section_array.data.line_angle.to_numpy()
         ),
         span_length=section_array.data.span_length.to_numpy(),
     )
 
-    # show_street_map(section_array, all_lats, all_lons)
+    show_street_map(section_array, all_lats, all_lons)
 
 
 def test_section_array_to_gps_2():
@@ -107,27 +107,21 @@ def test_section_array_to_gps_2():
         start_lat_deg=48.8566,  # in rads: 0.852708
         start_lon_deg=2.3522,  # in rads: 0.041053
         azimuth_deg=90,
-        line_angles_degrees=-np.degrees(
+        line_angles_degrees=np.degrees(
             section_array.data.line_angle.to_numpy()
         ),
         span_length=section_array.data.span_length.to_numpy(),
     )
 
-    # show_street_map(section_array, all_lats, all_lons)
+    show_street_map(section_array, all_lats, all_lons)
 
 
 def test_gps_to_section_array_1():
-    # Radius 6371 000
-    lats = np.degrees(
-        np.array(
-            [0.852708, 0.8527550884, 0.852816919, 0.8528880459, 0.8529546364]
-        )
+    lats = np.array(
+        [48.8566, 48.85929796, 48.8628406, 48.86691587, 48.87073122]
     )
-    lons = np.degrees(
-        np.array(
-            [0.041053, 0.041053, 0.0410695724, 0.0411199932, 0.0412212353]
-        )
-    )
+
+    lons = np.array([2.3522, 2.3522, 2.35125047, 2.34836157, 2.34256082])
 
     distances, angles = get_dist_and_angles_from_gps(lats, lons)
     np.testing.assert_allclose(
@@ -137,15 +131,11 @@ def test_gps_to_section_array_1():
 
 
 def test_gps_to_section_array_2():
-    lats = np.degrees(
-        np.array(
-            [0.852708, 0.8527079987, 0.8526970941, 0.8527039307, 0.8526795512]
-        )
+    lats = np.array(
+        [48.8566, 48.8565999273, 48.8559751397, 48.8563668445, 48.8549700038]
     )
-    lons = np.degrees(
-        np.array(
-            [0.041053, 0.0411245687, 0.0412185428, 0.0413373695, 0.0414756252]
-        )
+    lons = np.array(
+        [2.3522, 2.3480994121, 2.3427150917, 2.3359068169, 2.3279853474]
     )
     distances, angles = get_dist_and_angles_from_gps(lats, lons)
     np.testing.assert_allclose(
@@ -159,13 +149,13 @@ def test_gps_to_section_array_2():
 def test_round_trip():
     # --- Forward: section geometry → GPS ---
     # Put 0 at first and last support, because inverse operation can't manage them
-    line_angles = np.array([0.0, 10.0, -15.0, 20.0, 0])
+    line_angles = np.array([0.0, 10.0, -15.0, 20.0, 0])  # anticlockwise
     span_lengths = np.array([300.0, 400.0, 500.0, 600.0, np.nan])
 
     lats, lons = get_gps_from_arrays(
         start_lat_deg=48.8566,
         start_lon_deg=2.3522,
-        azimuth_deg=90.0,  # first span heads East
+        azimuth_deg=90.0,  # first span heads West
         line_angles_degrees=line_angles,
         span_length=span_lengths,
     )