fix vhl sign
Signed-off-by: WANG Jérémy <jeremy.wang@rte-france.com>

src/mechaphlowers/core/models/balance/models/model_ducloux.py
@@ -38,7 +38,7 @@ from mechaphlowers.core.models.cable.deformation import (
 from mechaphlowers.core.models.cable.span import CatenarySpan, ISpan
 from mechaphlowers.core.models.external_loads import CableLoads
 from mechaphlowers.entities.arrays import CableArray, SectionArray
-from mechaphlowers.entities.core import VhlStrength
+from mechaphlowers.entities.core import VhlResult
 from mechaphlowers.numeric import cubic
 from mechaphlowers.utils import arr
 
@@ -392,15 +392,18 @@ class BalanceModel(IBalanceModel):
         self.update_span()
         self.update_tensions()
 
-    # For the following vhl methods, the Fz component is reversed:
+    # For the following vhl methods, the Fz and Fy components are reversed:
     # the z axis is reversed (downwards) for users wanting vhl results
-    def vhl_under_chain(self) -> VhlStrength:
+    # the y axis is also reversed: toward oneself (right of the line)
+
+    # Dans VhlResult argument dans le init pour inverser le repère
+    def vhl_under_chain(self) -> VhlResult:
         Fx, Fy, Fz = self.nodes.vector_projection.force_cable()
         reversed_Fz = -(Fz + self.nodes.signed_counterweight)
-        vhl_result = np.array([reversed_Fz, Fy, Fx])
-        return VhlStrength(vhl_result, "N")
+        vhl_result = np.array([reversed_Fz, -Fy, Fx])
+        return VhlResult(vhl_result, "N")
 
-    def vhl_under_chain_left(self) -> VhlStrength:
+    def vhl_under_chain_left(self) -> VhlResult:
         """Get the VHL efforts under chain: without considering insulator_weight.
 
         VHL at the left of the support.
@@ -408,14 +411,14 @@ class BalanceModel(IBalanceModel):
         Format: [[V0, H0, L0], [V1, H1, L1], ...]
 
         Returns:
-            VhlStrength: VhlStrength object
+            VhlResult: VhlResult object
         """
         Fx, Fy, Fz = self.nodes.vector_projection.force_cable_left()
         reversed_Fz = -(Fz + self.nodes.signed_counterweight)
-        vhl_result = np.array([reversed_Fz, Fy, Fx])
-        return VhlStrength(vhl_result, "N")
+        vhl_result = np.array([reversed_Fz, -Fy, Fx])
+        return VhlResult(vhl_result, "N")
 
-    def vhl_under_chain_right(self) -> VhlStrength:
+    def vhl_under_chain_right(self) -> VhlResult:
         """Get the VHL efforts under chain: without considering insulator_weight.
 
         VHL at the right of the support.
@@ -423,22 +426,22 @@ class BalanceModel(IBalanceModel):
         Format: [[V0, H0, L0], [V1, H1, L1], ...]
 
         Returns:
-            VhlStrength: VhlStrength object
+            VhlResult: VhlResult object
         """
         Fx, Fy, Fz = self.nodes.vector_projection.force_cable_right()
         reversed_Fz = -(Fz + self.nodes.signed_counterweight)
-        vhl_result = np.array([reversed_Fz, Fy, Fx])
-        return VhlStrength(vhl_result, "N")
+        vhl_result = np.array([reversed_Fz, -Fy, Fx])
+        return VhlResult(vhl_result, "N")
 
-    def vhl_under_console(self) -> VhlStrength:
+    def vhl_under_console(self) -> VhlResult:
         Fx, Fy, Fz = self.nodes.vector_projection.force_cable()
         reversed_Fz = -(
             Fz
             + self.nodes.signed_insulator_weight
             + self.nodes.signed_counterweight
         )
-        vhl_result = np.array([reversed_Fz, Fy, Fx])
-        return VhlStrength(vhl_result, "N")
+        vhl_result = np.array([reversed_Fz, -Fy, Fx])
+        return VhlResult(vhl_result, "N")
 
     @property
     def has_loads(self) -> bool:

test/core/models/balance/models/test_model_ducloux_integration.py
@@ -329,6 +329,7 @@ def test_adjust_with_angles(
 
 
 @pytest.mark.integration
+# Also test vhl, contrary to the other tests
 def test_wind_no_altitude_change(
     section_array_no_altitude_change: SectionArray,
     cable_array_AM600: CableArray,
@@ -367,7 +368,7 @@ def test_wind_no_altitude_change(
     expected_vhl_under_console = np.array(
         [
             [5.34538576e02, 7.60729616e02, 6.72246587e02, 4.45064517e02],
-            [2.47336911e02, 3.72285443e02, 3.22182818e02, 2.01698053e02],
+            [-2.47336911e02, -3.72285443e02, -3.22182818e02, -2.01698053e02],
             [3.91179542e03, -4.89819371e00, -5.35288081e-01, -3.90633052e03],
         ]
     )
@@ -381,10 +382,10 @@ def test_wind_no_altitude_change(
                 345.064517160492,
             ],
             [
-                247.336910630502,
-                372.285442725628,
-                322.182818012918,
-                201.698053053279,
+                -247.336910630502,
+                -372.285442725628,
+                -322.182818012918,
+                -201.698053053279,
             ],
             [
                 3911.79537992953,
@@ -1033,11 +1034,11 @@ def test_angles_counterweight(cable_array_AM600: CableArray):
                 17119.90740813478,
                 3907.54805180944,
             ],
-            [  # reversed H values compared to proto
-                788.259742993468,
-                8093.4515419997,
-                19249.9553187584,
-                -665.25113770311,
+            [
+                -788.259742993468,
+                -8093.4515419997,
+                -19249.9553187584,
+                665.25113770311,
             ],
             [
                 35307.1946728045,

test/entities/test_core.py
@@ -8,7 +8,7 @@ from unittest.mock import patch
 
 import numpy as np
 
-from mechaphlowers.core.models.balance.interfaces import VhlStrength
+from mechaphlowers.core.models.balance.interfaces import VhlResult
 from mechaphlowers.entities.core import QuantityArray
 
 
@@ -17,7 +17,7 @@ def test_vhl_strength():
     arr = np.array(
         [[1000, 2000, 3000], [4000, 5000, 6000], [7000, 8000, 9000]]
     )
-    vhl = VhlStrength(arr, input_unit="N")
+    vhl = VhlResult(arr, input_unit="N")
 
     V, H, L = vhl.vhl
     np.testing.assert_array_equal(V.value(), np.array([1000, 2000, 3000]) / 10)
@@ -32,14 +32,14 @@ def test_vhl_strength():
 @patch('mechaphlowers.config.options.output_units.force', 'daN')
 def test_vhl_strength_str_repr():
     arr = np.array([[1000, 2000], [3000, 4000], [5000, 6000]])
-    vhl = VhlStrength(arr, input_unit="N")
+    vhl = VhlResult(arr, input_unit="N")
     vhl_str = str(vhl)
     expected_str = (
         "V: [100. 200.] daN\nH: [300. 400.] daN\nL: [500. 600.] daN\n"
     )
     assert vhl_str == expected_str
     vhl_repr = repr(vhl)
-    expected_repr = f"VhlStrength\n{expected_str}"
+    expected_repr = f"VhlResult\n{expected_str}"
     assert vhl_repr == expected_repr
 
 