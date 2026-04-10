8b1d41d2996c166e5f9dac1b241418febfa3047e
ai-qui <184963772+ai-qui@users.noreply.github.com>
2026-04-10 11:43:58 +0200

    Fix measured_solar_irradiance -> measured_global_radiation
    Signed-off-by: ai-qui <184963772+ai-qui@users.noreply.github.com>

--- src/mechaphlowers/core/models/cable/thermal.py
+++ src/mechaphlowers/core/models/cable/thermal.py
@@ -324,7 +324,7 @@ class ThermalEngine:
         )
 
         self.dict_input = {
-            "measured_solar_irradiance": inputs["solar_irradiance"],
+            "measured_global_radiation": inputs["solar_irradiance"],
             "latitude": inputs["latitude"],
             "longitude": inputs["longitude"],
             "altitude": inputs["altitude"],
